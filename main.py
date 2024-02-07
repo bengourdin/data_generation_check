17dbb876a0a48015f4d114bba5a657086fefd5b9
Benoît Gourdin <benoit.gourdin@pwc.com>
2024-02-07 16:30:16 +0100

    check more distribution

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from scipy.stats._stats import _kendall_dis
 from fitter import Fitter, get_common_distributions, get_distributions
 
 
+# read config of wanted statistics
 def read_config(path):
     with open(path, 'r') as f:
         config = json.load(f)
@@ -19,6 +20,7 @@ def read_config(path):
     return parameters, variables
 
 
+# manual computation of somersd between risk driver (x) and binary label (y)
 def calculate_somersd(x, y):
     x = np.asarray(x).ravel()
     y = np.asarray(y).ravel()
@@ -50,8 +52,9 @@ def calculate_somersd(x, y):
     return (SD, dis)
 
 
+# check data column for different distributions and return the most probable
 def get_distribution(data_array):
-    f = Fitter(data_array, distributions=["norm", "expon"]) # check the scipy distribution
+    f = Fitter(data_array, distributions=["alpha", "beta", "gamma", "logistic", "uniform", "norm", "expon"]) # distribution to check
     f.fit()
     best_json = f.get_best(method= 'sumsquare_error')
     best_array = []
@@ -59,8 +62,25 @@ def get_distribution(data_array):
         best_array.append(best)
     return best_array[0]
 
+
+ # All possible distributions
+ # ['alpha', 'anglit', 'arcsine', 'argus', 'beta', 'betaprime', 'bradford', 'burr', 'burr12', 'cauchy',
+ # 'chi', 'chi2', 'cosine', 'crystalball', 'dgamma', 'dweibull', 'erlang', 'expon', 'exponnorm', 'exponpow',
+ # 'exponweib', 'f', 'fatiguelife', 'fisk', 'foldcauchy', 'foldnorm', 'gamma', 'gausshyper', 'genexpon',
+ # 'genextreme', 'gengamma', 'genhalflogistic', 'genhyperbolic', 'geninvgauss', 'genlogistic', 'gennorm',
+ # 'genpareto', 'gilbrat', 'gompertz', 'gumbel_l', 'gumbel_r', 'halfcauchy', 'halfgennorm', 'halflogistic',
+ # 'halfnorm', 'hypsecant', 'invgamma', 'invgauss', 'invweibull', 'johnsonsb', 'johnsonsu', 'kappa3',
+ # 'kappa4', 'ksone', 'kstwo', 'kstwobign', 'laplace', 'laplace_asymmetric', 'levy', 'levy_l', 'levy_stable',
+ # 'loggamma', 'logistic', 'loglaplace', 'lognorm', 'loguniform', 'lomax', 'maxwell', 'mielke', 'moyal',
+ # 'nakagami', 'ncf', 'nct', 'ncx2', 'norm', 'norminvgauss', 'pareto', 'pearson3', 'powerlaw',
+ # 'powerlognorm', 'powernorm', 'rayleigh', 'rdist', 'recipinvgauss', 'reciprocal', 'rice', 'rv_continuous',
+ # 'rv_histogram', 'semicircular', 'skewcauchy', 'skewnorm', 'studentized_range', 't', 'trapezoid', 'trapz',
+ # 'triang', 'truncexpon', 'truncnorm', 'tukeylambda', 'uniform', 'vonmises', 'vonmises_line', 'wald',
+ # 'weibull_max', 'weibull_min', 'wrapcauchy']
+
+
 def main(input_dir_path, output_dir_path, dataset):
-    print(get_distributions()) # distributions supported by scipy
+    #print(get_distributions()) # distributions supported by scipy
     generated_dataset = pd.read_csv(os.path.join(input_dir_path, dataset), sep=",")
     wanted_correlation = pd.read_csv(os.path.join(input_dir_path, 'Correlationmatrix.csv'), sep=";", decimal=",")
     parameters, variables = read_config(os.path.join(input_dir_path, 'Statistische_Informationen.json'))
@@ -73,14 +93,17 @@ def main(input_dir_path, output_dir_path, dataset):
     new_correlation = generated_dataset.corr()
     corr_difference = new_correlation.subtract(wanted_correlation)
     corr_difference = corr_difference.abs()
+    # correlation difference (difference between wanted corr and achieved corr) csv
     csv_dataset = corr_difference.to_csv(os.path.join(output_dir_path, 'correlation_difference.csv'), sep=';', decimal=',')
-    json_file = {}
+    # correlation difference heatmap
     plt.figure()
     ax = sns.heatmap(corr_difference, annot=True, vmin=0, vmax=1)
     plt.savefig(os.path.join(output_dir_path, 'correlation_difference.png'), bbox_inches = "tight")
     somersd_value = []
     somersd_type = []
     labels = []
+    json_file = {}
+    # computation of all statistical information and differences with the wanted one
     for col in generated_dataset.columns:
         distribution = get_distribution(generated_dataset[col].values)
         median = round(generated_dataset[col].median(), 3)
@@ -134,10 +157,12 @@ def main(input_dir_path, output_dir_path, dataset):
             labels.append(col)
             json_file[col] = {"median": str(median), "median_should": median_should, "median_difference": str(median_dif), "min": str(min), "min_should": min_should, "min_difference": str(min_dif), "max": str(max), "max_should": max_should, "max_difference": str(max_dif), "q25": str(q25), "q25_should": q25_should, "q25_difference": str(q25_dif), "q75": str(q75), "q75_should": q75_should, "q75_difference": str(q75_dif), "somers'd": str(somersd_calc), "somers'd_should": somersd_should, "somers'd_difference": str(somersd_dif), "distribution": distribution}
     data = pd.DataFrame({'labels': labels, 'value': somersd_value, 'type': somersd_type})
+    # somers'd bar plot
     plt.figure()
     ax = sns.barplot(data=data, x='labels', y='value', hue='type', hue_order=['actual', 'target', 'difference'])
     plt.xticks(rotation=90)
     plt.savefig(os.path.join(output_dir_path, 'somers_d.png'), bbox_inches = "tight")
+    # json of data statistics
     with open(os.path.join(output_dir_path, 'statistics_of_generated.json'), "w") as write_file:
         json.dump(json_file, write_file, indent=4)
 
